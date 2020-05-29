c7bd02e8fc36148053a8f5e75f3b27356d67dd5f
Md. Asadouzzaman <30689970+AsadDUET@users.noreply.github.com>
2020-05-29 12:42:53 +0600

    'new'

---
+++ attendence.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 12:35:40 2020
+
+@author: Azad
+"""
+import
+
+
+load employee data
+
+def
+
+
+while True:
+    punched='check entry punch'
+    if punchde:
+        wait
+        scan QR
+        scan Face
+        Polition ir
+        take temperature
+        save data
+    punched2='check for exit punch'
+    if punched2:
+        wait
+        scan QR
+        save dara
+    if time full:
+        email
+        
+
